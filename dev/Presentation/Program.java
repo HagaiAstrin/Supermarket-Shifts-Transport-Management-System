a4a0e5e6cc36de7fd19f98ae81eb5cfdbc4a30b1
asafzenou <asafzenou97@gmail.com>
2024-05-26 13:31:11 +0300

    After a SUCCESS in login to Admin

--- dev/Presentation/Program.java
+++ dev/Presentation/Program.java
@@ -71,7 +71,9 @@ public class Program {
         String userName = scanner.nextLine();
         System.out.print("Please enter your password: ");
         String password = scanner.nextLine();
-        return new String[]{userName, password};
+        System.out.print("Please enter your ID: ");
+        String id = scanner.nextLine();
+        return new String[]{userName, password, id};
     }
 
     private void Logout(){
@@ -111,7 +113,7 @@ public class Program {
                     break;
                 case 4:
                     // Call method to update employee details
-                    Menu.UpdateEmployeeDetails();
+//                    Menu.UpdateEmployeeDetails();
                     break;
                 case 5:
                     return;
