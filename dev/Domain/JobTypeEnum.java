df912aa453cb17d96429fd45d1cf66a54bbc80c7
HagaiAstrin <hagai156@gmail.com>
2024-05-30 18:51:55 +0300

    18:51

--- dev/Domain/JobTypeEnum.java
+++ dev/Domain/JobTypeEnum.java
@@ -2,23 +2,8 @@ package Domain;
 
 public enum JobTypeEnum {
 
-    SHIFT_MANAGER(){
-        @Override
-        public String toString(){
-            return "Shift Manager";
-        }
-    },
-    CASHIER(){
-        @Override
-        public String toString(){
-            return "Cashier";
-        }
-    },
-    STOCK_KEEPER(){
-        @Override
-        public String toString(){
-            return "Stock Manager";
-        }
-    },
+    SHIFT_MANAGER,
+    CASHIER,
+    STOCK_KEEPER,
     GUARD
 }
