32ed6998197e9ce7ada31d7c7f8f42c7b1cec8db
Yonisegall <yonisegall@gmail.com>
2024-06-02 21:51:19 +0300

    02/06 - 21:51

--- dev/domain/Solutions.java
+++ dev/domain/Solutions.java
@@ -14,23 +14,24 @@ public class Solutions {
     public static JsonObject get_Truck_Json() {
         JsonObject new_json = new JsonObject();
         int count = 1;
-        for(Truck tr : DataStructManager.trucks){
-            if(tr.isAvailability() && (tr.getMax_weight() - tr.getNet_weight()) >= DataStructManager.current_max_transport){
+        for (Truck tr : DataStructManager.trucks) {
+            if (tr.isAvailability() && (tr.getMax_weight() - tr.getNet_weight()) >= DataStructManager.current_max_transport) {
                 new_json.addProperty(String.valueOf(count), tr.to_String());
                 count++;
             }
         }
         return new_json;
     }
+
     /**
      * @param a - String argument represent the Site to dropped items from
      * @return JsonObject of the Items to dropped
      */
-    public static JsonObject get_Item_Json(String a){
+    public static JsonObject get_Item_Json(String a) {
         JsonObject new_json = new JsonObject();
         int count = 1;
-        for(Document d : DataStructManager.documents) {
-            if(d.to_string().equals(a)) {
+        for (Document d : DataStructManager.documents) {
+            if (d.to_string().equals(a)) {
                 for (Map.Entry<Item, Integer> iter : d.getItem_map().entrySet()) {
                     new_json.addProperty(String.valueOf(count++), d.item_String(iter.getKey()));
                 }
@@ -39,26 +40,28 @@ public class Solutions {
         }
         return new_json;
     }
+
     /**
-     *
      * @return JsonObject represent the available Sites to dropped
      */
-    public static JsonObject Choose_Drop_Target(){
+    public static JsonObject Choose_Drop_Target() {
         JsonObject j = new JsonObject();
         int count = 1;
-        for (Document d: DataStructManager.documents){
+        for (Document d : DataStructManager.documents) {
             j.addProperty(String.valueOf(count++), d.to_string());
         }
         return j;
     }
+
     /**
      * Replace Documents
+     *
      * @param a - String represent of the Document to replace
      */
-    public static void replace_documents(String a){
+    public static void replace_documents(String a) {
         int count = 0;
-        for(Document d : documents){
-            if(d.to_string().equals(a)){
+        for (Document d : documents) {
+            if (d.to_string().equals(a)) {
                 documents.remove(d);
                 Document doc = documents.remove(documents.size() - 1);
                 documents.add(count, doc);
@@ -67,55 +70,69 @@ public class Solutions {
             count++;
         }
     }
+
     /**
      * Dropped Items
+     *
      * @param a - String represent of the Item to dropped
      */
-    public static void drop_Items(String a){
+    public static void drop_Items(String a) {
         boolean bool = false;
-        for(Document d : documents) {
+        for (Document d : documents) {
             for (Map.Entry<Item, Integer> entry : d.getItem_map().entrySet()) {
                 if (d.item_String(entry.getKey()).equals(a)) {
                     d.drop_Item(entry.getKey());
-                    if(d.getItem_map().isEmpty()) documents.remove(d);
+                    if (d.getItem_map().isEmpty()) documents.remove(d);
                     all_items.remove(entry.getKey());
                     bool = true;
                     break;
                 }
             }
-            if(bool) break;
+            if (bool) break;
         }
     }
+
     /**
      * Dropped Site from Database
+     *
      * @param a - String argument represent the Site to dropped
      */
-    public static void drop_Site(String a){
-        for(Document d : documents){
-            if(d.to_string().equals(a)){
-                for(Map.Entry<Item, Integer> entry : d.getItem_map().entrySet()){
-                    items.remove(entry.getKey());
-                    all_items.remove(entry.getKey());
-                }
+    public static void drop_Site(String a) {
+        int count = 0;
+        for (Document d : documents) {
+            if (d.to_string().equals(a)) {
                 documents.remove(d);
-                if (d.getTarget().getType().equals("Supplier")){
-                    for (Map.Entry<Item, Integer> iter : d.getItem_map().entrySet()){
-                        for(Document doc : documents){
-                            if(doc == d) break;
-                            if(d.getTarget().getType().equals("Store")) {
-                                for (Map.Entry<Item, Integer> item : doc.getItem_map().entrySet()) {
+                if (d.getTarget().getType().equals("Supplier")) {
+                    for (Map.Entry<Item, Integer> iter : d.getItem_map().entrySet()) {
+                        all_items.put(iter.getKey(), all_items.get(iter.getKey()) - iter.getValue());
+                        for (int i = count; i < documents.size(); i++) {
+                            if (documents.get(i).getTarget().getType().equals("Store")) {
+                                for (Map.Entry<Item, Integer> item : documents.get(i).getItem_map().entrySet()) {
                                     if (item.getKey().to_string().equals(iter.getKey().to_string())) {
-                                        doc.getItem_map().remove(item.getKey());
+                                        documents.get(i).getItem_map().remove(item.getKey());
                                     }
                                 }
                             }
                         }
+                        items.remove(iter.getKey());
+                    }
+                } else {
+                    for (Map.Entry<Item, Integer> iter : d.getItem_map().entrySet()) {
+                        all_items.put(iter.getKey(), all_items.get(iter.getKey()) + iter.getValue());
+                        for (int i = 0; i < count; i++) {
+                            if (documents.get(i).getTarget().getType().equals("Supplier")) {
+                                for (Map.Entry<Item, Integer> item : documents.get(i).getItem_map().entrySet()) {
+                                    if (item.getKey().to_string().equals(iter.getKey().to_string())) {
+                                        item.setValue(item.getValue() + iter.getValue());
+                                    }
+                                }
+                            }
+                        }
+                        items.remove(iter.getKey());
                     }
                 }
-                else{
-
-                }
             }
+            count++;
         }
     }
 }
