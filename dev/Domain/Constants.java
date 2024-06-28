beea2fb898ac9162262f9146421f90466fdd86db
HagaiAstrin <154841517+HagaiAstrin@users.noreply.github.com>
2024-06-28 20:01:07 +0300

    Delete dev directory

--- dev/Domain/Constants.java
+++
@@ -1,9 +0,0 @@
-package Domain;
-
-public class Constants {
-    public static final String DEV = "dev\\Data\\";
-    public static final String PATH_EMPLOYEE = "\\Employees.csv";
-    public static final String PATH_DATA_VALIDATION_ADMIN = "\\DataValidationAdmin.csv";
-    public static final String PATH_DATA_VALIDATION_USER = "\\DataValidationUser.csv";
-    public static final String PATH_DATA_PREFERENCES = "\\Preferences\\";
-}
